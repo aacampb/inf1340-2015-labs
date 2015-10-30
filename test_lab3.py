91cb747c47cf7b82487f35ebb3010544b1a7ff12
aacampb <aaronl.campbell@mail.utoronto.ca>
2015-10-29 20:41:12 -0400

    working on lab

--- test_lab3.py
+++ test_lab3.py
@@ -13,7 +13,6 @@ MONTHS_WITH_31 = ["January", "March", "May", "July", "August", "October", "Decem
 MONTHS_WITH_30 = ["April", "June", "September", "November"]
 MONTHS_WITH_28_or_29 = ["February"]
 
-
 def test_months_with_31():
     """
     Test months with 31 days
@@ -23,11 +22,32 @@ def test_months_with_31():
 
 # Write a test function for the months with 30 days
 
+def test_months_with_30():
+    """
+    Test month with 30 days
+    """
+    for item in MONTHS_WITH_30:
+        assert days_in_month(item) == 30
 
 # Write a test function for the months with 28 or 29 days
 
+def test_months_with_28_or_29():
+    """
+    Test for months with 28 or 29 days
+    """
+    for item in MONTHS_WITH_28_or_29:
+        assert days_in_month(item) == "28 or 29"
 
 # Write a test function for months that are not capitalized properly
+
+def test_months_not_capitalized():
+    """
+    Test for months that are not capitalized properly
+    """
+
+    for item in MONTHS_WITH_31 or MONTHS_WITH_30 or MONTHS_WITH_28_or_29:
+        assert d
+
 # Hint: use the lower() string method
 
 # Write a test function for unexpected input
