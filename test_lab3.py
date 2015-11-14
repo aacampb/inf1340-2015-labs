ac2be49e61f263dba46e06352fac07c019121c92
aacampb <aaronl.campbell@mail.utoronto.ca>
2015-11-14 16:51:46 -0500

    lab completed

--- test_lab3.py
+++ test_lab3.py
@@ -12,45 +12,60 @@ from lab3 import days_in_month
 MONTHS_WITH_31 = ["January", "March", "May", "July", "August", "October", "December"]
 MONTHS_WITH_30 = ["April", "June", "September", "November"]
 MONTHS_WITH_28_or_29 = ["February"]
+UNEXPECTED_INPUT = ["Christmas", 3.14, "25", 42, "hasldhsd"]
+
 
 def test_months_with_31():
-    """
-    Test months with 31 days
-    """
-    for item in MONTHS_WITH_31:
-        assert days_in_month(item) == 31
+	"""
+	Test months with 31 days
+	"""
+	for item in MONTHS_WITH_31:
+		assert days_in_month(item) == 31
 
 # Write a test function for the months with 30 days
 
+
 def test_months_with_30():
-    """
-    Test month with 30 days
-    """
-    for item in MONTHS_WITH_30:
-        assert days_in_month(item) == 30
+	"""
+	Test month with 30 days
+	"""
+	for item in MONTHS_WITH_30:
+		assert days_in_month(item) == 30
 
 # Write a test function for the months with 28 or 29 days
 
+
 def test_months_with_28_or_29():
-    """
-    Test for months with 28 or 29 days
-    """
-    for item in MONTHS_WITH_28_or_29:
-        assert days_in_month(item) == "28 or 29"
+	"""
+	Test for months with 28 or 29 days
+	"""
+	for item in MONTHS_WITH_28_or_29:
+		assert days_in_month(item) == "28 or 29"
 
 # Write a test function for months that are not capitalized properly
 
+
 def test_months_not_capitalized():
-    """
-    Test for months that are not capitalized properly
-    """
+	"""
+	Test for months that are not capitalized properly
+	"""
 
-    for item in MONTHS_WITH_31 or MONTHS_WITH_30 or MONTHS_WITH_28_or_29:
-        assert d
+	for item in MONTHS_WITH_31 or MONTHS_WITH_30 or MONTHS_WITH_28_or_29:
+		item = item.lower()
+		assert days_in_month(item) == 31 or 30 or "28 or 29"
 
-# Hint: use the lower() string method
 
-# Write a test function for unexpected input
-# Hint: use a try/except block to deal with the exception
-# Hint: use data types other than strings as input
+def test_unexpected_input():
+	"""
+	Test for unexpected input
+	"""
 
+	try:
+		for item in UNEXPECTED_INPUT:
+			days_in_month(item)
+	except ValueError:
+		assert True
+	except AttributeError:
+		assert True
+	except TypeError:
+		assert True
